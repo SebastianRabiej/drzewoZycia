34f5d19e7c89893f2d33e50fd2fb0cb90d0b3162
immabeee <lukasz.kordiak@gmail.com>
2018-01-02 14:29:19 +0100

    Kolizja z kolcami

---
+++ Assets/scripts/Kolizja.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Kolizja : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision collision)
+    {
+        if (collision.collider.GetComponent<CharacterControllerMove>() != null)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
